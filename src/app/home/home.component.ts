f1b68e3688fcd1ce1c3adef0199097541c49fe64
Your Name <you@example.com>
2018-06-28 12:27:12 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,62 @@
+// this is a by default statement
+import { Component, OnInit } from '@angular/core';
+
+// decorator
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+// a simple class
+export class HomeComponent implements OnInit {
+
+  //declare a dummy blog variable here
+  public allBlogs = [
+    {
+      "blogId": "1",
+      "lastModified": "2018-06-20T12:20:47.854Z",
+      "created": "2018-06-20T12:20:47.854Z",
+      "tags": [],
+      "author": "Admin",
+      "category": "Comedy",
+      "isPublished": true,
+      "views": 1000,
+      "bodyHtml": "this is blog body",
+      "description": "this is blog 1 description",
+      "title": "This is blog 1"
+    },
+    {
+      "blogId": "2",
+      "lastModified": "2018-06-20T12:20:47.854Z",
+      "created": "2018-06-20T12:20:47.854Z",
+      "tags": [],
+      "author": "Admin",
+      "category": "Comedy",
+      "isPublished": true,
+      "views": 1000,
+      "bodyHtml": "<h1>this is a big text </h1> <p>Small text </p>",
+      "description": "this is the description of example blog and is edited.",
+      "title": "This is an example blog"
+    },
+    {
+      "blogId": "3",
+      "lastModified": "2018-06-20T12:20:47.854Z",
+      "created": "2018-06-20T12:20:47.854Z",
+      "tags": [],
+      "author": "Admin",
+      "category": "Comedy",
+      "isPublished": true,
+      "views": 1000,
+      "bodyHtml": "this is the blog body. this is the blog body. this is the blog body. this is the blog body.",
+      "description": "this is the third blog description",
+      "title": "This is blog 3"
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
